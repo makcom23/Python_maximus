6c58b72b1096050dc393a4d770068fdaa4227d74
makcom23 <dynatek.m@gmail.com>
2025-03-18 18:35:55 +0200

    По моему не работает функция getPoints()...

--- Прямоугольник.py
+++ Прямоугольник.py
@@ -8,11 +8,11 @@ class Figure:
         self.width=random.randint(10, 100)
         self.counter=random.randint(3, 10)
         self.center_x=random.randint(self.width, global_x)
-        self.center_y=random.randint(self.width, global_y)
+        self.center_y=random.randint(self.height, global_y)
 
     def getPoint(self):
-        x_point = random.randint(self.center_x-self.width/2, self.center_x+self.width/2)
-        y_point = random.randint(self.center_y-self.height/2, self.center_y+self.height/2)
+        x_point = random.randint(int(self.center_x-self.width/2), int(self.center_x+self.width/2))
+        y_point = random.randint(int(self.center_y-self.height/2), int(self.center_y+self.height/2))
         return x_point, y_point
     
     def getPoints(self):
@@ -25,7 +25,11 @@ class Figure:
 
 figure=Figure(1000,1000)
 
-x_coord, y_coord=figure.getPoints()
+points=figure.getPoints()
+# по идее мы имеем список кортежей, который вернули обращением к методу getPoints()
+# но согласно print - у нас выводится только 2 значения... WTF
+print(points)
+x_coord, y_coord = zip(*points)
 
 plt.plot(x_coord, y_coord, marker="o", linestyle="-", color="b", label="Quadro")
 
