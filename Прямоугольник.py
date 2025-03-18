11059233fb23d02a9551347a8c170142da097f04
makcom23 <dynatek.m@gmail.com>
2025-03-18 17:00:34 +0200

    Строка 18-22: сделал список и добавил точки х,у в список , согласно counter

--- Прямоугольник.py
+++ Прямоугольник.py
@@ -3,28 +3,31 @@ import random
 import math
 
 class Figure:
-    def __init__(self, lenght, width):
-        self.lenght=lenght
-        self.width=width
+    def __init__(self, global_x, global_y):
+        self.height=random.randint(10, 100)
+        self.width=random.randint(10, 100)
+        self.counter=random.randint(3, 10)
+        self.center_x=random.randint(self.width, global_x)
+        self.center_y=random.randint(self.width, global_y)
 
     def getPoint(self):
-        x_point = [0, 0, self.lenght, self.lenght, 0]
-        y_point = [0, self.width, self.width, 0, 0]
+        x_point = random.randint(self.center_x-self.width/2, self.center_x+self.width/2)
+        y_point = random.randint(self.center_y-self.height/2, self.center_y+self.height/2)
         return x_point, y_point
+    
+    def getPoints(self):
+        points = []
+        for _ in range(self.counter):
+            points.append(self.getPoint())
+            return points
 
 
-lenght=random.randint(-10, 10)
-width=random.randint(-10, 10)
 
-if lenght==0:
-    lenght=lenght+1
-else: 
-    width=width+1 
+figure=Figure(1000,1000)
 
-rect=Figure(lenght,width)
-
-x_coord, y_coord=rect.getPoint()
+x_coord, y_coord=figure.getPoints()
 
 plt.plot(x_coord, y_coord, marker="o", linestyle="-", color="b", label="Quadro")
 
 plt.show()
+
